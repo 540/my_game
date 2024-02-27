70dedb2bd8460b084ac49fb4956f084a51deee54
Stefan Karasmilov <stefan.karasmilov@540deg.com>
2024-02-27 08:45:03 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flame/flame.dart';
+import 'package:flame/game.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:my_game/my_game.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Flame.device.fullScreen();
+  await Flame.device.setLandscape();
+
+  MyGame game = MyGame();
+
+  runApp(GameWidget(game: kDebugMode ? MyGame() : game));
+}
